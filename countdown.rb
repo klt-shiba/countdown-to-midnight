80d27e6d9727adf4c9604f66ac6131fe36288bab
Colin Tseng <klt.aus@yahoo.com>
2021-06-21 19:26:25 +1000

    Completed assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(x)
+    while x >= 1 
+        puts "#{x} SECOND(S)!"
+        x -= 1 
+    end 
+    return  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(x)
+    sleep(5)
+    while x >= 1 
+        puts "#{x} SECOND(S)!"
+        x -= 1 
+    end 
 end
